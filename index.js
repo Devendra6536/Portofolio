76048344e31a3afb42a989264636bc67665a31ea
Devendra kumar <devendrapal8682@gmail.com>
2022-07-27 00:19:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// this is resume container
+function ResumeOpen(){
+    document.getElementById("resume").style.display = "flex";
+    document.getElementById("resume").style.zIndex= "2";
+    document.body.style.backgroundColor = "grey";
+    document.body.style.zIndex = "-1";
+}
+function ResumeClose(){
+    document.getElementById("resume").style.display = "none";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+// this is project container
+function ProjectOpen(){
+    document.getElementById("project").style.display = "flex";
+    document.getElementById("project").style.zIndex= "2";
+    document.body.style.zIndex = "-1";
+}
+function ProjectClose(){
+    document.getElementById("project").style.display = "none";
+}
+
+
+// this is contact container
+function ContactOpen(){
+    document.getElementById("contact").style.display = "flex";
+}
+function ContactClose(){
+    document.getElementById("contact").style.display = "none";
+}
